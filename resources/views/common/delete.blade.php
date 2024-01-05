6a90a2024bcb576ddca2ed5bb1b459069900534c
suzukijin <suzukijin@localhost.localdomain>
2024-01-05 17:37:23 +0900

    latest

--- resources/views/common/delete.blade.php
+++ resources/views/common/delete.blade.php
@@ -10,9 +10,9 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <div class="modal-body text-center">
+      <div class="modal-body text-left ml-5 font-weight-bold">
       <div class="mb-3">
-            <p data-user-id="{{$sample->id}}"></p>
+            
       </div>
       <div class="mb-3">
             Name : <span id="data_name"></span>
@@ -25,13 +25,24 @@
       </div>
       </div>
       <div class="modal-footer">
-      <form method="POST" action="/index/delete">
+      <form method="POST" action="/index/delete" onsubmit="return showAlert()">
         @csrf
         @method('DELETE')
        
-        <button type="submit" class="btn btn-primary" id="pass_to_modal" onclick="showAlert()">confirm delete</button>
+        <button type="submit" class="btn btn-primary" id="pass_to_modal" >confirm delete</button>
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
       </form>
+      <script>
+      function showAlert() {
+        var confirmation = confirm("本当に削除しますか？");
+        if (confirmation) {    
+            alert("処理が完了しました！");
+            return true;
+        } else {
+            return false;
+        }
+      }
+      </script>
       </div>
     </div>
   </div>
